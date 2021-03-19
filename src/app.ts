b1ceae01731cb900cb3d4d2d55605183248555ad
Kevin Fatkin <kfatkin@gmail.com>
2021-03-19 11:13:54 -0400

    Cleanup import statements

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,3 @@
-/// <reference path="./lib/models/drag-drop.ts" />
-/// <reference path="./lib/models/project.ts" />
-/// <reference path="./lib/state/project-state.ts" />
-/// <reference path="./lib/util/validation.ts" />
-/// <reference path="./lib/decorators/autobind.ts" />
-/// <reference path="./lib/components/project-item.ts" />
 /// <reference path="./lib/components/project-input.ts" />
 /// <reference path="./lib/components/project-list.ts" />
 
